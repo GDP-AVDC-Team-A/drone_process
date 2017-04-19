629ad3dbdc49ad9d872969970eb29e8dbfa39ee2
Alberto Camporredondo <alberto.camporredondop@alumnos.upm.es>
2017-04-19 11:57:53 +0200

    Changed maintainer

--- include/drone_process.h
+++ include/drone_process.h
@@ -4,20 +4,20 @@
  *  \details    This file contains the DroneProcess declaration. To obtain more information about
  *              it's definition consult the drone_process.cpp file.
  *  \authors    Enrique Ortiz, Yolanda de la Hoz, Martin Molina, David Palacios
- *  \copyright  Copyright 2016 Universidad Politecnica de Madrid (UPM) 
+ *  \copyright  Copyright 2016 Universidad Politecnica de Madrid (UPM)
  *
- *     This program is free software: you can redistribute it and/or modify 
- *     it under the terms of the GNU General Public License as published by 
- *     the Free Software Foundation, either version 3 of the License, or 
- *     (at your option) any later version. 
- *   
- *     This program is distributed in the hope that it will be useful, 
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of 
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- *     GNU General Public License for more details. 
- *   
- *     You should have received a copy of the GNU General Public License 
- *     along with this program. If not, see http://www.gnu.org/licenses/. 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see http://www.gnu.org/licenses/.
  ********************************************************************************/
 
 #ifndef DRONE_PROCESS
